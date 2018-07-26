c2bf68f38288dde014970ddfd62f41e7131050e7
HugoGustavo <hugog.valin@gmail.com>
2018-07-26 08:50:57 -0300

    Multiple Resolver Mapping

--- src/com/tutorialspoint/HelloController.java
+++ src/com/tutorialspoint/HelloController.java
@@ -1,16 +1,16 @@
 package com.tutorialspoint;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.AbstractController;
-
-public class HelloController extends AbstractController {
-	@Override
-	protected ModelAndView handleRequestInternal(HttpServletRequest request, HttpServletResponse reponse) throws Exception {
-		ModelAndView model = new ModelAndView("hello");
-		model.addObject("message", "Hello World!");
-		return model;
+@Controller
+@RequestMapping("/hello")
+public class HelloController {
+	@RequestMapping(method = RequestMethod.GET)
+	public String printHello(ModelMap model) {
+		model.addAttribute("message", "Hello Spring MVC Framework!");
+		return "hello";
 	}
 }
